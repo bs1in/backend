1e06f684a5790480cb53476f05ec578dad306d84
Admin <Admin@Z217-PC05>
2019-04-11 14:42:48 +0200

    kleine Änderung

--- api/index.php
+++ api/index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "Ich bin da MICHI"
+        echo "Ich bin da MICHI und LEON"
         // put your code here
         ?>
     </body>
