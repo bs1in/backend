2bec27689d0be1352d32ba46e2b0531e932deb4e
Backend11d <backend11d@byom.de>
2019-05-23 14:09:22 +0200

    Spring added

---
+++ src/main/java/de/bs1/Inventarisierung/InventarisierungApplication.java
@@ -0,0 +1,13 @@
+package de.bs1.Inventarisierung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarisierungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarisierungApplication.class, args);
+	}
+
+}
